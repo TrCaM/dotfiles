d69262ead506838076fb6ed90780edb7695173d4
Tri Cao <minhtricao1996@gmail.com>
2025-08-02 13:39:40 -0400

    Fixing sketchybar

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -2,12 +2,15 @@
 
 source "$CONFIG_DIR/colors.sh"
 
-FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
-
-if [ "$NAME" = "space.$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.color=$ACCENT_PRIMARY \
-                         label.color=$BACKGROUND_1
-else
-  sketchybar --set $NAME background.color=$BACKGROUND_1 \
-                         label.color=$ACCENT_PRIMARY
+if [ "$SENDER" = "aerospace_workspace_change" ]; then
+  FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
+  for sid in $(aerospace list-workspaces --all); do
+    if [ "$sid" = "$FOCUSED_WORKSPACE" ]; then
+      sketchybar --set space.$sid background.color=$ACCENT_PRIMARY \
+                             label.color=$BACKGROUND_1
+    else
+      sketchybar --set space.$sid background.color=$BACKGROUND_1 \
+                             label.color=$ACCENT_PRIMARY
+    fi
+  done
 fi
