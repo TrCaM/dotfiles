d8ecd9f2863c4fd69d8faef237e12e398746929d
Tri Cao <minhtricao1996@gmail.com>
2025-08-03 00:13:04 -0400

    haha

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -7,50 +7,19 @@
 
 sketchybar --add event aerospace_workspace_change
 
-for m in $(aerospace list-monitors | awk '{print $1}'); do
-  for i in $(aerospace list-workspaces --monitor $m); do
-    sid=$i
-    space=(
-      space="$sid"
-      icon="$sid"
-      icon.highlight_color=$RED
-      icon.padding_left=10
-      icon.padding_right=10
-      display=$m
-      padding_left=2
-      padding_right=2
-      label.drawing=off
-      background.color=$BACKGROUND_1
-      script="$PLUGIN_DIR/space.sh"
-    )
-
-    sketchybar --add space space.$sid left \
-               --set space.$sid "${space[@]}" \
-               --subscribe space.$sid mouse.clicked
-  done
+for sid in $(aerospace list-workspaces --all); do
+  sketchybar --add item space.$sid left \
+    --subscribe space.$sid aerospace_workspace_change \
+    --set space.$sid \
+    icon="$sid" \
+    icon.highlight_color=$RED \
+    icon.padding_left=10 \
+    icon.padding_right=10 \
+    display=$m \
+    padding_left=2 \
+    padding_right=2 \
+    label.drawing=off \
+    background.color=$BACKGROUND_1 \
+    click_script="aerospace workspace $sid" \
+    script="$CONFIG_DIR/plugins/aerospace.sh $sid"
 done
-
-
-space_creator=(
-  icon=􀆊
-  icon.font="$FONT:Heavy:16.0"
-  padding_left=10
-  padding_right=8
-  label.drawing=off
-  display=active
-  #click_script='yabai -m space --create'
-  script="$PLUGIN_DIR/space_windows.sh"
-  # script="$PLUGIN_DIR/aerospace.sh"
-  icon.color=$WHITE
-)
-
-# sketchybar --add item space_creator left               \
-#            --set space_creator "${space_creator[@]}"   \
-#            --subscribe space_creator space_windows_change
-sketchybar --add item space_creator left               \
-           --set space_creator "${space_creator[@]}"   \
-           --subscribe space_creator aerospace_workspace_change
-
-# sketchybar  --add item change_windows left \
-#             --set change_windows script="$PLUGIN_DIR/change_windows.sh" \
-#             --subscribe change_windows space_changes
--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-source "$CONFIG_DIR/colors.sh"
+# make sure it's executable with:
+# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
-if [ "$SENDER" = "aerospace_workspace_change" ]; then
-  FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
-  for sid in $(aerospace list-workspaces --all); do
-    if [ "$sid" = "$FOCUSED_WORKSPACE" ]; then
-      sketchybar --set space.$sid background.color=$ACCENT_PRIMARY \
-                             label.color=$BACKGROUND_1
-    else
-      sketchybar --set space.$sid background.color=$BACKGROUND_1 \
-                             label.color=$ACCENT_PRIMARY
-    fi
-  done
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+  sketchybar --set $NAME background.drawing=on
+else
+  sketchybar --set $NAME background.drawing=off
 fi
