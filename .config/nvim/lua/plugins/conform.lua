c96c24e9debe5ce5120f8ae968580e01b91ebbac
Tri Cao <minhtricao1996@gmail.com>
2025-08-08 10:18:11 -0400

    Another config push

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -7,10 +7,5 @@ return {
       vue = { "prettier" },
       markdown = { "prettierd" },
     },
-    format_on_save = {
-      -- These options will be passed to conform.format()
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
   },
 }
