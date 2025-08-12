72c7460efb17afd21df07c57c6216fea4490960a
Tri Cao <minhtricao1996@gmail.com>
2025-08-11 23:55:20 -0400

    Finish pokemon things

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -7,16 +7,38 @@ return {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
       -- {
-      sections = {
-        {
-          section = "terminal",
-          cmd = "/usr/local/bin/pokemon-colorscripts -n snorlax --no-title; sleep .1",
-          height = 20,
-          indent = 3,
-        },
-        { section = "keys", gap = 1, padding = 1 },
-        { section = "startup" },
-      },
+      sections = (function()
+        math.randomseed(os.time())
+        local pokemons = {
+          "pikachu",
+          "snorlax",
+          "bulbasaur",
+          "squirtle",
+          "blastoise",
+          "gengar",
+          "psyduck",
+          "dragonite",
+        }
+        local small_pokemons = {
+          pikachu = true,
+          bulbasaur = true,
+          squirtle = true,
+        }
+        local chosen_pokemon = pokemons[math.random(#pokemons)]
+        local height = small_pokemons[chosen_pokemon] and 15 or 20
+
+        return {
+          {
+            section = "terminal",
+            cmd = "/usr/local/bin/pokemon-colorscripts -n " .. chosen_pokemon .. " --no-title; sleep .1",
+            random = 8,
+            height = height,
+            indent = 3,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+        }
+      end)(),
     },
   },
 }
